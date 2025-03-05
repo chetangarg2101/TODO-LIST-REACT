84c1fd472e0eab156b8969e4fe2a4fd0b47777e4
chetangarg2101 <chetangarg2101@gmail.com>
2025-03-05 04:52:52 +0000

    asa

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import React, { useState } from 'react'
-import "./App.css"
-import TodoInput from './components/TodoInput'
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import TodoInput from './components/TodoInput';
 import Todolist from './components/TodoList';
+
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem = (key)=>{
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+  // Load saved todos from localStorage when the component mounts
+  const [listTodo, setListTodo] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  // Save todos to localStorage whenever listTodo changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(listTodo));
+  }, [listTodo]);
+
+  const addList = (inputText) => {
+    if (inputText.trim() !== '') {
+      setListTodo((prevTodos) => [...prevTodos, inputText]);
+    }
+  };
+
+  const deleteListItem = (index) => {
+    setListTodo((prevTodos) => prevTodos.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput addList={addList}/>
+        <TodoInput addList={addList} />
         <h1 className="app-heading">TODO</h1>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
-          return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-          )
-        })}
+        <hr />
+        {listTodo.map((listItem, i) => (
+          <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
